cef6e3fb5f9e50fe915a6d952a41fd256a6f58f5
DeniArif <deniarifwibowo82@gmail.com>
2024-10-12 21:57:33 +0700

    1

---
+++ app/src/main/java/com/example/mytugas/Variabel.kt
@@ -0,0 +1,12 @@
+package com.example.mytugas
+
+fun main() {
+    // Byte
+    val ByteValue: Byte = 10
+    println("Byte: $ByteValue")
+
+
+    // Int
+    val intValue: Int = 30
+    println(intValue)
+}
