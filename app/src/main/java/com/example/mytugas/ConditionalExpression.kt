7eab4c56d4f12abcd8e94f7e79d13c71170431db
DeniArif <deniarifwibowo82@gmail.com>
2024-10-12 22:37:04 +0700

    16 (ConditionalExpression)

--- app/src/main/java/com/example/mytugas/ConditionalExpression.kt
+++ app/src/main/java/com/example/mytugas/ConditionalExpression.kt
@@ -46,5 +46,21 @@ fun Perulangan(){
     for (car in cars){
         println("Car type: $car")
     }
+    var x = 1
+    while (x <= 5) {
+       println("Perulangan ke-$x")
+       x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
 }
 
